2b8112c0f9b09182b612337e1415d642bdcc1e39
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2015-04-05 04:54:16 +0900

    remove unused init files.

--- ipadic/__init__.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, parent_dir)
-
-from dic import SystemDictionary
-SYS_DIC = SystemDictionary("ipadic")
