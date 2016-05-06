0b801d7e49ad17825e716681cb225cf238cbd0e3
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2016-05-06 13:21:19 +0900

    Update package version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if not os.path.exists('sysdic') and os.path.exists(os.path.join('ipadic', 'sysdi
     with ZipFile(os.path.join(dicdir, 'sysdic.zip')) as zf:
         zf.extractall()
 
-version = '0.2.7'
+version = '0.2.8-dev'
 name = 'janome'
 short_description = '`janome` is a package for Japanese Morphological Analysis.'
 long_description = """\
