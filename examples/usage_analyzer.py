454187a053d546b96e468fc0f090e5cdc4e7e254
mocobeta <tomoko.uchida.1111@gmail.com>
2017-08-06 12:20:18 +0900

    Add TokenCountFilter

--- examples/usage_analyzer.py
+++ examples/usage_analyzer.py
@@ -7,6 +7,7 @@ from janome.tokenfilter import *
 import logging
 logging.basicConfig(level='INFO')
 
+print(u'Analyzer example:')
 text = u'蛇の目はPure Ｐｙｔｈｏｎな形態素解析器です。'
 char_filters = [UnicodeNormalizeCharFilter(), RegexReplaceCharFilter(u'蛇の目', u'janome')]
 tokenizer = Tokenizer()
@@ -14,3 +15,11 @@ token_filters = [CompoundNounFilter(), LowerCaseFilter()]
 a = Analyzer(char_filters, tokenizer, token_filters)
 for token in a.analyze(text):
     print(token)
+
+print('')
+print(u'Analyzer example: Count nouns with POSKeepFilter and TokenCountFilter')
+text = u'すもももももももものうち'
+token_filters = [POSKeepFilter('名詞'), TokenCountFilter()]
+a = Analyzer(token_filters=token_filters)
+for k, v in a.analyze(text):
+    print('%s: %d' % (k, v))
