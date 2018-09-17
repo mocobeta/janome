769ad2c831efb8dd3b905eef31f5fd1a66815a00
Tomoko Uchida <tomoko.uchida@uzabase.com>
2018-09-17 18:17:48 +0900

    Add credit, limitation notes.

--- examples/usage_with_zipimport.py
+++ examples/usage_with_zipimport.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
-"""zipアーカイブから直接ロードする方法
+u"""
+This was contributed by @roy-freee.
+
+zipアーカイブから直接ロードする方法
 
 準備: wheelを作る(手動でパッケージをzip圧縮してもいい)
 $ pip wheel . --no-deps --no-binary
 もしくは、
 $ python setup.py bdist_wheel --universal
 出来上がった.whlファイルを使う
+
+[制限事項] mmap=True オプションとは併用できません。(i.e. NEologd 同梱 janome に zip import は適用できません。)
 
 How to import the zip archive
 
@@ -14,6 +19,8 @@ $ pip wheel . --no-deps --no-binary
 $ python setup.py bdist_wheel --universal
 
 You can also create a zip archived package by yourself.
+
+[Limitation] mmap=True option cannot be used with zip importing.
 """
 
 import sys
