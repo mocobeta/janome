23d84b7c1a9215850e14faea886d6f54bf73b4a4
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-12-08 23:16:59 +0900

    Add 'dotfile' option to output graphviz dotfile.

--- janome/lattice.py
+++ janome/lattice.py
@@ -49,6 +49,9 @@ class Node(object):
                 self.infl_type, self.infl_form, self.base_form, self.reading, self.phonetic,
                 self.back_pos, self.back_index)
 
+    def node_label(self):
+        return self.surface
+
 
 class SurfaceNode(object):
     """
@@ -65,6 +68,9 @@ class SurfaceNode(object):
 
         self.num, self.surface, self.left_id, self.right_id, self.cost = dict_entry
         self.node_type = node_type
+
+    def node_label(self):
+        return self.surface
 
 
 class BOS(object):
@@ -83,6 +89,9 @@ class BOS(object):
     def __str__(self):
         return '__BOS__'
 
+    def node_label(self):
+        return 'BOS'
+
 
 class EOS(object):
     """
@@ -96,6 +105,9 @@ class EOS(object):
 
     def __str__(self):
         return '__EOS__' + ' [back_pos=%d]' % self.back_pos
+
+    def node_label(self):
+        return 'EOS'
 
 class Lattice:
     def __init__(self, size, dic):
@@ -146,6 +158,58 @@ class Lattice:
             pos = node.back_pos
         path.reverse()
         return path
+
+    # generate Graphviz dot file
+    def generate_dotfile(self, filename='lattice.gv'):
+
+        def is_unknown(node):
+            return hasattr(node, 'node_type') and node.node_type == NodeType.UNKNOWN
+
+        # traverse lattice and make nodes and edges
+        node_ids = []
+        edges = []
+        path = self.backward()
+        for pos in range(0, len(self.snodes) - 1):
+            for i in range(0, len(self.snodes[pos])):
+                node1 = self.snodes[pos][i]
+                if is_unknown(node1):
+                    continue
+                node1_id = (pos, i)
+                if node1_id not in node_ids:
+                    node_ids.append(node1_id)
+                node_len = len(node1.surface) if hasattr(node1, 'surface') else 1
+                for j in range(0, len(self.snodes[pos + node_len])):
+                    node2 = self.snodes[pos + node_len][j]
+                    if is_unknown(node2):
+                        continue
+                    node2_id = (pos+node_len, j)
+                    if node2_id not in node_ids:
+                        node_ids.append(node1_id)
+                    edges.append((node1_id, node2_id))
+
+        # output dot file
+        with open(filename, 'w') as f:
+            f.write('digraph G {\n')
+            f.write('  rankdir=LR;\n')
+            f.write('  ranksep=2.0;\n')
+            for node_id in node_ids:
+                (pos, idx) = node_id
+                node = self.snodes[pos][idx]
+                id_str = '%d.%d' % (pos, idx)
+                label = '%s\\n%s' % (node.node_label(), str(node.cost))
+                shape = 'ellipse' if isinstance(node, BOS) or isinstance(node, EOS) else 'box'
+                color = 'lightblue' if isinstance(node, BOS) or isinstance(node, EOS) or node in path else 'lightgray'
+                f.write('  %s [label="%s",shape=%s,style=filled,fillcolor=%s];\n' % (id_str, label, shape, color))
+            for edge in edges:
+                ((pos1, idx1), (pos2, idx2)) = edge
+                node1 = self.snodes[pos1][idx1]
+                node2 = self.snodes[pos2][idx2]
+                id_str1 = '%d.%d' % (pos1, idx1)
+                id_str2 = '%d.%d' % (pos2, idx2)
+                label = str(self.dic.get_trans_cost(node1.right_id, node2.left_id))
+                (color, style) = ('blue', 'bold') if node1 in path and node2 in path else ('black', 'solid')
+                f.write('  %s -> %s [label="%s",color=%s,style=%s,fontcolor=red];\n' % (id_str1, id_str2, label, color, style))
+            f.write('}\n')
 
     def __str__(self):
         return '\n'.join(','.join(str(node) for node in nodes) for nodes in self.snodes)
