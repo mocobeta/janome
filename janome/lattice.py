36797481884365809996f21dca61ea990371ed3e
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2020-08-21 00:39:01 +0900

    Avoid tuple unpacking

--- janome/lattice.py
+++ janome/lattice.py
@@ -62,13 +62,21 @@ class Node(NodeBase):
         return self.surface
 
 
-class SurfaceNode(NodeBase):
+class SurfaceNode(object):
     """
     Node class with surface form only.
     """
+    __slots__ = [
+        'pos', 'index', 'min_cost', 'back_pos', 'back_index',
+        'num', 'surface', 'left_id', 'right_id', 'cost', 'node_type'
+    ]
 
     def __init__(self, dict_entry, node_type=NodeType.SYS_DICT):
-        super().__init__()
+        self.pos = 0
+        self.index = 0
+        self.min_cost = 2147483647  # int(pow(2,31)-1)
+        self.back_pos = -1
+        self.back_index = -1
         self.num, self.surface, self.left_id, self.right_id, self.cost = dict_entry
         self.node_type = node_type
 
@@ -76,16 +84,23 @@ class SurfaceNode(NodeBase):
         return self.surface
 
 
-class BOS(NodeBase):
+class BOS(object):
     """
     BOS node
     """
+    __slots__ = [
+        'pos', 'index', 'min_cost', 'back_pos', 'back_index',
+        'right_id', 'cost'
+    ]
 
     def __init__(self):
-        super().__init__()
+        self.pos = 0
+        self.index = 0
+        self.min_cost = 0
+        self.back_pos = -1
+        self.back_index = -1
         self.right_id = 0
         self.cost = 0
-        self.min_cost = 0
 
     def __str__(self):
         return '__BOS__'
@@ -94,14 +109,21 @@ class BOS(NodeBase):
         return 'BOS'
 
 
-class EOS(NodeBase):
+class EOS(object):
     """
     EOS node
     """
+    __slots__ = [
+        'pos', 'index', 'min_cost', 'back_pos', 'back_index',
+        'left_id', 'cost'
+    ]
 
     def __init__(self, pos):
-        super().__init__()
         self.pos = pos
+        self.index = 0
+        self.min_cost = 2147483647  # int(pow(2,31)-1)
+        self.back_pos = -1
+        self.back_index = -1
         self.cost = 0
         self.left_id = 0
 
