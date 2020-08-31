257a3ab6c350efe8b3b46794c9d13502b6065487
uezo <uezo@uezo.net>
2020-08-31 22:48:16 +0900

    Add test around progress indicator to `tests/test_dic.py`

--- tests/test_dic.py
+++ tests/test_dic.py
@@ -24,6 +24,7 @@ from janome.dic import (
     FILE_USER_FST_DATA,
     FILE_USER_ENTRIES_DATA
 )
+from janome.progress import SimpleProgressIndicator, logger as p_logger
 
 # TODO: better way to find package...
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,6 +137,40 @@ class TestDictionary(unittest.TestCase):
         compiled_user_dic = CompiledUserDictionary(dic_dir, connections=connections)
         self.assertEqual(1, len(compiled_user_dic.lookup('とうきょうスカイツリー駅'.encode('utf8'))))
 
+        # create user dictionary from csv with progress indicator
+        progress_indicator = SimpleProgressIndicator(update_frequency=1.0)
+        with self.assertLogs(logger=p_logger) as cm:
+            # create user dictionary
+            large_user_dic = UserDictionary(
+                user_dict=os.path.join(parent_dir, 'tests/user_ipadic.csv'),
+                enc='utf8', type='ipadic', connections=connections,
+                progress_handler=progress_indicator)
+
+            entry_count = len(large_user_dic.entries)
+            # output for each entry and for complete (entry_count + 1)
+            self.assertEqual((entry_count + 1) * 2, len(cm.output))
+            # reset after complete
+            self.assertIsNone(progress_indicator.value)
+
+            for i in range(0, (entry_count + 1) * 2):
+                if i < entry_count:
+                    # progress for reading csv
+                    self.assertIn('Reading user dictionary from CSV', cm.output[i])
+                    self.assertIn(f'{i + 1}/{entry_count}', cm.output[i])
+                elif i == entry_count:
+                    # on compete loading csv
+                    self.assertIn(f'{entry_count}/{entry_count}', cm.output[i])
+                elif i < entry_count * 2 + 1:
+                    # progress for create_minimum_transducer
+                    self.assertIn('Running create_minimum_transducer', cm.output[i])
+                    self.assertIn(f'{i - entry_count}/{entry_count}', cm.output[i])
+                elif i == entry_count * 2 + 1:
+                    # on compete loading create_minimum_transducer
+                    self.assertIn(f'{entry_count}/{entry_count}', cm.output[i])
+
+        # same result as without progress indicator
+        self.assertEqual(1, len(large_user_dic.lookup('東京スカイツリー'.encode('utf8'))))
+
     def test_simplified_user_dictionary(self):
         # create user dictionary from csv
         user_dic = UserDictionary(user_dict=os.path.join(parent_dir, 'tests/user_simpledic.csv'),
@@ -151,6 +186,40 @@ class TestDictionary(unittest.TestCase):
         # load compiled dictionary
         compiled_user_dic = CompiledUserDictionary(dic_dir, connections=connections)
         self.assertEqual(1, len(compiled_user_dic.lookup('とうきょうスカイツリー駅'.encode('utf8'))))
+
+        # create simplified user dictionary from csv with progress indicator
+        progress_indicator = SimpleProgressIndicator(update_frequency=1.0)
+        with self.assertLogs(logger=p_logger) as cm:
+            # create user dictionary
+            large_user_dic = UserDictionary(
+                user_dict=os.path.join(parent_dir, 'tests/user_simpledic.csv'),
+                enc='utf8', type='simpledic', connections=connections,
+                progress_handler=progress_indicator)
+
+            entry_count = len(large_user_dic.entries)
+            # output for each entry and for complete (entry_count + 1)
+            self.assertEqual((entry_count + 1) * 2, len(cm.output))
+            # value is reset after complete
+            self.assertIsNone(progress_indicator.value)
+
+            for i in range(0, (entry_count + 1) * 2):
+                if i < entry_count:
+                    # progress for reading csv
+                    self.assertIn('Reading user dictionary from CSV', cm.output[i])
+                    self.assertIn(f'{i + 1}/{entry_count}', cm.output[i])
+                elif i == entry_count:
+                    # on compete loading csv
+                    self.assertIn(f'{entry_count}/{entry_count}', cm.output[i])
+                elif i < entry_count * 2 + 1:
+                    # progress for create_minimum_transducer
+                    self.assertIn('Running create_minimum_transducer', cm.output[i])
+                    self.assertIn(f'{i - entry_count}/{entry_count}', cm.output[i])
+                elif i == entry_count * 2 + 1:
+                    # on compete loading create_minimum_transducer
+                    self.assertIn(f'{entry_count}/{entry_count}', cm.output[i])
+
+        # same result as without progress indicator
+        self.assertEqual(1, len(large_user_dic.lookup('東京スカイツリー'.encode('utf8'))))
 
 
 if __name__ == '__main__':
