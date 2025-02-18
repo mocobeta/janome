37d5c45b2117010ac557db0fc99092d6a22a803e
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2025-02-18 17:57:20 +0900

    update doc structure

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -11,3 +11,7 @@ pip install -r requirements-docs.txt
 cd ${BASEDIR}/api && make clean && make html
 cd ${BASEDIR}/ja && make clean && make html
 cd ${BASEDIR}/en && make clean && make html
+
+mkdir -p ${BASEDIR}/build/reference && cp -Rp ${BASEDIR}/api/_build/html/* ${BASEDIR}/build/reference
+mkdir -p ${BASEDIR}/build/ja && cp -Rp ${BASEDIR}/ja/_build/html/* ${BASEDIR}/build/ja
+mkdir -p ${BASEDIR}/build/en && cp -Rp ${BASEDIR}/en/_build/html/* ${BASEDIR}/build/en
