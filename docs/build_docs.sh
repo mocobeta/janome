e283fdff0c6991f43df0e30476a32ae110198372
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2025-02-19 17:40:40 +0900

    fix build script

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR=$(cd $(dirname $0) && pwd)
 cd $BASEDIR
@@ -15,9 +15,9 @@ cd ${BASEDIR}/en && make clean && make html
 
 rm -rf ${BASEDIR}/build
 mkdir -p ${BASEDIR}/build
+cp -Rp ${BASEDIR}/api/_build/html/ ${BASEDIR}/build/reference
 cp -Rp ${BASEDIR}/ja/_build/html/ ${BASEDIR}/build/ja
 cp -Rp ${BASEDIR}/en/_build/html/ ${BASEDIR}/build/en
-cp -Rp ${BASEDIR}/api/_build/html/ ${BASEDIR}/build/reference
+cp ${BASEDIR}/index.html ${BASEDIR}/build/index.html
 
 echo "All build finished. Assets are in ${BASEDIR}/build"
-  
